b04494c4d30942be8606ac06001ba819653c6f08
Singh Tamang <chandan.singh.tamang@accenture.com>
2020-11-02 10:04:04 +0530

    adding all

---
+++ currency-exchange-service/src/main/java/com/ctech/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,33 @@
+package com.ctech.microservice.currencyexchangeservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ctech.microservice.currencyexchangeservice.model.ExchangeValue;
+import com.ctech.microservice.currencyexchangeservice.repository.ExchangeValueRepository;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	Environment environment;
+	
+	@Autowired
+	ExchangeValueRepository repository;
+
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		ExchangeValue findByFromAndTo = repository.findByFromAndTo(from, to);
+		
+		
+		findByFromAndTo.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
+	}
+}
---
+++ currency-exchange-service/src/main/java/com/ctech/microservice/currencyexchangeservice/repository/ExchangeValueRepository.java
@@ -0,0 +1,11 @@
+package com.ctech.microservice.currencyexchangeservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ctech.microservice.currencyexchangeservice.model.ExchangeValue;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
